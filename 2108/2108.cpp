51ee54924228bdf39b5e3ed69ab5eb9cf2093d07
32132 <dsa@gmail.com>
2024-08-21 18:25:41 +0200

    123

--- 2108/2108.cpp
+++ 2108/2108.cpp
@@ -1,6 +1,50 @@
 ﻿#include <iostream>
 using namespace std;
-class 
+class Pryamo{
+private:
+	int x;
+	int y;
+	void Proverka() {
+		bool Tf;
+		if (x > 0 && y > 0) {
+			bool Tf = false;
+		}
+		else {
+			bool Tf = true;
+		}
+	}
+public:
+	int GetX() {
+		return x;
+	}
+	int GetY() {
+		return y;
+	}
+	void SetY(int valueY) {
+		y = valueY;
+	}
+	void SetX(int valueX) {
+		x = valueX;
+	}
+	int Perimetr() {
+		Proverka();
+		int summa;
+		summa = x + y + x + y;
+		return summa;
+	}
+	int Plochad() {
+		Proverka();
+		int summa;
+		summa = x * y;
+		return summa;
+	}
+	void PlochadVivod() {
+		cout << "Plochad: " << Plochad();
+	}
+	void PerimetrVivod() {
+		cout << "Perimetr: " << Perimetr();
+	}
+};
 int main()
 {
    
